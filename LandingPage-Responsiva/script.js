0b3daf70507240344ce508119239761f1b6dc3bf
Isabella Rodrigues <bebellabr@gmail.com>
2023-06-19 20:09:27 -0300

    fix final

--- LandingPage-Responsiva/script.js
+++ LandingPage-Responsiva/script.js
@@ -6,17 +6,20 @@ menu.addEventListener('click', () => {
     NavMenu.classList.toggle('ativo');
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    var perguntas = document.querySelectorAll('.pergunta-titulo');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var perguntas = document.querySelectorAll('.pergunta-titulo');
 
-    perguntas.forEach(function(pergunta) {
-    pergunta.addEventListener('click', function() {
-        var descricao = this.nextElementSibling;
-        descricao.style.display = (descricao.style.display === 'none') ? 'block' : 'none';
-    });
-    });
-});
+//     perguntas.forEach(function(pergunta) {
+//     pergunta.addEventListener('click', function() {
+//         var descricao = this.nextElementSibling;
+//         descricao.style.display = (descricao.style.display === 'none') ? 'block' : 'none';
+//     });
+//     });
+// });
 
 function togglePergunta(elemento) {
     elemento.parentElement.classList.toggle('aberta');
+    const desc = elemento.nextElementSibling;
+    console.log(desc.style.display);
+    desc.style.display = desc.style.display === 'none' || desc.style.display === ''  ? 'block' : 'none';
 }
