81cf8e2bad2dbeb0ab0001fcf8db5ce81c7bb552
Isabella Rodrigues <bebellabr@gmail.com>
2024-02-02 02:32:31 -0300

    feat: test dashboard

---
+++ LandingPage-Responsiva/ordersdashboard.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Usuário 1',
+        productNumber: '85743',
+        paymentStatus: 'Processando',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Usuário 2',
+        productNumber: '97245',
+        paymentStatus: 'Pago',
+        status: 'Rejeitado'
+    },
+    {
+        productName: 'Usuário 3',
+        productNumber: '36452',
+        paymentStatus: 'Processando',
+        status: 'Pendente'
+    },
+]
